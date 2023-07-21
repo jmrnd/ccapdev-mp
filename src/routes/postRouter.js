84e6037e85747d323b409c2378a553001ab04401
senkco <129177159+senkco@users.noreply.github.com>
2023-07-21 11:12:38 +0800

    added view-post

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -9,6 +9,7 @@ const postRouter = Router();
 postRouter.use(bodyParser.urlencoded({ extended: true}));
 postRouter.use(bodyParser.json());
 
+
 postRouter.get("/create-post", async (req, res) => {
     try {
         const session = await UserSession.findOne({});
@@ -38,7 +39,6 @@ postRouter.post('/create_post', async (req, res) => {
         const currentUser = await User.findOne({ _id: session.userID });
       // Extract data from the form
       const { title, text } = req.body;
-        console.log(req.body);
       // Create a new Post instance
       const newPost = new Post({
         title,
@@ -60,5 +60,43 @@ postRouter.post('/create_post', async (req, res) => {
       res.status(500).send('Error saving post to the database.');
     }
   });
+
+
+postRouter.get('/view-post', async (req, res) =>{
+  try{
+    const session = await UserSession.findOne({});
+    const currentUser = await User.findOne({ _id: session.userID });
+    const getPost = await Post.findOne({title:"Check out this awesome HTML5 trick!"});
+    const postOP = await User.findOne({ _id: getPost.author}); 
+    let bool = false;
+      if(postOP.username === currentUser.username ){
+        bool = true;
+      }
+
+      if(getPost){
+        res.render("view-post", {
+          equal: bool,
+          icon: postOP.icon,
+          postOP: postOP.username,
+          title: getPost.title,
+          text: getPost.body,
+          votes: getPost.totalVotes,
+          numcomments: getPost.totalComments,
+          currentuser: currentUser.username
+        })
+
+      } else {
+        // No post found
+        console.log("No post found");
+        // To redirect to an error page
+        res.status(404).send("Post not found");
+    }
+
+  }
+  catch (error) {
+    console.error("Error occurred while retrieving user:", error);
+    res.status(500).send("Internal Server Error"); // To redirect to an error page
+  }
+});
 
   export default postRouter;
