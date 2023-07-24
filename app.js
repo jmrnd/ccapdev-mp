2fbd8177e6a86d795cc6de80f0cd17eabaca59c2
jmrnd <jamarlancemiranda@gmail.com>
2023-07-24 14:55:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import express from "express";
 import exphbs from "express-handlebars";
 
 // Routes modules
-import router from "./src/routes/index.js";
+import router from "./src/routes/indexRouter.js";
 
 // Custom hbs helpers
 import customHelpers from "./src/hbs-helpers/helpers.js";
@@ -20,30 +20,30 @@ mongoose.connect(mongoURI).then(() => console.log("Connected to DB!"));
 //mongoose.connect('mongodb://127.0.0.1/foroom').then(() => console.log("Connected to DB!"));
 
 async function main() {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  const app = express();
-  app.use("/static", express.static(__dirname + "/public"));
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const app = express();
+    app.use("/static", express.static(__dirname + "/public"));
 
-  var hbs = exphbs.create({
-    helpers: customHelpers,
-    defaultLayout: "main",
-    partialsDir: __dirname + "/src/views/partials",
-    extname: "hbs",
-  });
+    var hbs = exphbs.create({
+        helpers: customHelpers,
+        defaultLayout: "main",
+        partialsDir: __dirname + "/src/views/partials",
+        extname: "hbs",
+    });
 
-  app.engine("hbs", hbs.engine);
-  app.set("view engine", "hbs");
-  app.set("views", "./src/views");
-  app.set("view cache", false);
+    app.engine("hbs", hbs.engine);
+    app.set("view engine", "hbs");
+    app.set("views", "./src/views");
+    app.set("view cache", false);
 
-  // Receive in JSON format
-  app.use(express.json());
+    // Receive in JSON format
+    app.use(express.json());
 
-  app.use(router);
+    app.use(router);
 
-  app.listen(process.env.SERVER_PORT, () => {
-    console.log("Hello! Listening at localhost:3000");
-  });
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log("Hello! Listening at localhost:3000");
+    });
 }
 
 main();
