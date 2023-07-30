594b264cc8e46eae49d78db8d494e979f8f915d7
jmrnd <jamarlancemiranda@gmail.com>
2023-07-30 18:23:20 +0800

    Update comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,68 +1,66 @@
+// EDIT COMMENT
+const editCommentBtns = document.querySelectorAll(".edit-comment-btn");
+const deleteCommentBtns = document.querySelectorAll(".delete-comment-btn");
 
-  // Get all the edit comment buttons
-  const editCommentBtns = document.querySelectorAll(".edit-comment-btn");
-  const deleteCommentBtns = document.querySelectorAll(".delete-comment-btn");
+// Add click event listener to each edit comment button
+editCommentBtns.forEach(event => {
+event.addEventListener("click", function (e) {
+    e.preventDefault();
 
-  // Add click event listener to each edit comment button
-  editCommentBtns.forEach(event => {
-    event.addEventListener("click", function (e) {
-      e.preventDefault();
+    // we get the body where the text and try to replace it with an input
+    const commentText = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".comment-text")
+    const commentInput = document.createElement("textarea"); // create input element
+    commentInput.classList.add("edit-text-area");
+    commentInput.setAttribute("rows", "5"); // Set the number of rows (you can change this value)
+    commentInput.setAttribute("cols", "20"); // Set the number of columns (you can change this value)
+    commentInput.value = commentText.textContent.trim();
 
-      // we get the body where the text and try to replace it with an input
-      const commentText = this.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector(".comment-text")
-      const commentInput = document.createElement("textarea"); // create input element
-      commentInput.classList.add("edit-text-area");
-      commentInput.setAttribute("rows", "5"); // Set the number of rows (you can change this value)
-      commentInput.setAttribute("cols", "20"); // Set the number of columns (you can change this value)
-      commentInput.value = commentText.textContent.trim();
+    // Replace the comment text with the input field
+    commentText.replaceWith(commentInput);
 
-      // Replace the comment text with the input field
-      commentText.replaceWith(commentInput);
+    // Show the input field and focus on it
+    commentInput.style.display = "block";
+    commentInput.focus();
 
-      // Show the input field and focus on it
-      commentInput.style.display = "block";
-      commentInput.focus();
+    const get_id = e.target.id;
 
-      const get_id = e.target.id;
+    // Add event listener to save changes on Enter key press or blur
+    commentInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        // Save changes and update the comment text
+        commentText.textContent = this.value;
+        // Replace the input field with the updated comment text
+        this.replaceWith(commentText);
 
-      // Add event listener to save changes on Enter key press or blur
-      commentInput.addEventListener("keydown", function (e) {
-        if (e.key === "Enter") {
-          // Save changes and update the comment text
-          commentText.textContent = this.value;
-          // Replace the input field with the updated comment text
-          this.replaceWith(commentText);
-
-          try{
+        try {
             const paramId = get_id.split("/"); // In array
 
             const data = {
-              postId: paramId[1],
-              commentId: paramId[2],
-              updateComment: commentText.textContent,
+                postId: paramId[1],
+                commentId: paramId[2],
+                updateComment: commentText.textContent,
             }
 
             const res = fetch(get_id, {
-              method: 'PATCH',
-              headers: {
+                method: 'PATCH',
+                headers: {
                 'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(data), // parse ID
+                },
+                body: JSON.stringify(data), // parse ID
             })
-
-          }catch(err){
+        } catch (err) {
             console.log(err);
-          }
-
-
-          const notifyEdited = document.createElement("p");
-          notifyEdited.textContent = "Edited";
-          notifyEdited.classList.add("edited-notify");
-          commentText.insertAdjacentElement("afterend",notifyEdited);
         }
-      });
+
+
+        const notifyEdited = document.createElement("p");
+        notifyEdited.textContent = "Edited";
+        notifyEdited.classList.add("edited-notify");
+        commentText.insertAdjacentElement("afterend",notifyEdited);
+    }
     });
-  });
+});
+});
 
 /*** DELETE COMMENT ***/
  deleteCommentBtns.forEach(event => {
--- src/models/Comment.js
+++ src/models/Comment.js
@@ -5,8 +5,8 @@ const CommentSchema = new mongoose.Schema({
     post: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
     body: String,
     commentDate: Date,
+    editDate: Date,
     totalVotes: Number,
-    isEdited: Boolean,
     replies: [{type: mongoose.Schema.Types.ObjectId, ref: "Comment"}], // array of comments, self-referencing
     upVoters: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     downVoters: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
