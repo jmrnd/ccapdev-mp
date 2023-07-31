f1a5b26a87858a98b91c12ed95a339ddca01d79e
jmrnd <jamarlancemiranda@gmail.com>
2023-07-31 20:03:01 +0800

    Update votes.js

--- public/js/votes.js
+++ public/js/votes.js
@@ -1,38 +1,32 @@
 let upvoteBtn = document.querySelectorAll('.postUpvote');
 let downvoteBtn = document.querySelectorAll('.postDownvote');
 
-upvoteBtn.forEach(button => {
-    button.addEventListener("click", async event => {
-        console.log("I am here");
-        const clickedButtonId = event.target.id; // upvoteIcon/{{this._id}}
-        const data = {
-          _id: clickedButtonId.substring(10) // {{this._id}}
-        }
+upvoteBtn.forEach(button => {button.addEventListener("click", async event => {
+    console.log("I am here");
+    const clickedButtonId = event.target.id; // upvoteIcon/{{this._id}}
+    const data = {
+        _id: clickedButtonId.substring(10) // {{this._id}}
+    }
 
-        const path = "/" + clickedButtonId; // construct path
+    const path = "/" + clickedButtonId; // construct path
 
-        try{
-          const res = await fetch(path, { // /upvoteIcon/{{this._id}}
+    try {
+        const res = await fetch(path, { // /upvoteIcon/{{this._id}}
             method: 'PATCH',
             headers: {
-              'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(data), // parse ID
-          })
-          if (res.status === 200) {
+        })
+        if (res.status === 200) {
             location.reload(); // Refresh page
-          }
         }
+    } catch (error) {
+        console.error('Error:', error);
+    };
+})});
 
-        catch(error){
-          console.error('Error:', error);
-        };
-      });
-    });
-
-
-downvoteBtn.forEach(button => {
-button.addEventListener("click", async event => {
+downvoteBtn.forEach(button => {button.addEventListener("click", async event => {
     console.log("I am here");
     const clickedButtonId = event.target.id; // upvoteIcon/{{this._id}}
     const data = {
@@ -42,83 +36,71 @@ button.addEventListener("click", async event => {
     const path = "/" + clickedButtonId; // construct path
 
     try{
-    const res = await fetch(path, { // /upvoteIcon/{{this._id}}
-        method: 'PATCH',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data), // parse ID
-    })
-        location.reload();
-
-        if (res.status === 200) {
-        location.reload(); // Refresh page
-        }
-    }
-
-    catch(error){
-    console.error('Error:', error);
-    };
-});
-});
-
-let commentUpvoteBtn = document.querySelectorAll('.commentUpvote');
-let commentDownvoteBtn = document.querySelectorAll('.commentDownvote');
-
-commentUpvoteBtn.forEach(button => {
-    button.addEventListener("click", async event => {
-        console.log("I am here");
-        const clickedButtonId = event.target.id; // upvoteIcon/{{this._id}}
-        const data = {
-          _id: clickedButtonId.substring(17) // {{this._id}}
-        }
-
-        const path = "/" + clickedButtonId; // construct path
-
-        console.log(path);
-        try{
-          const res = await fetch(path, {
-            method: 'PATCH',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data), // parse ID
-          })
-          if (res.status === 200) {
-            location.reload(); // Refresh page
-          }
-        }
-        catch(error){
-          console.error('Error:', error);
-        };
-      });
-    });
-
-
-commentDownvoteBtn.forEach(button => {
-    button.addEventListener("click", async event => {
-        console.log("I am here");
-        const clickedButtonId = event.target.id; // upvoteIcon/{{this._id}}
-        const data = {
-        _id: clickedButtonId.substring(18) // {{this._id}}
-        }
-
-        const path = "/" + clickedButtonId; // construct path
-
-        try{
         const res = await fetch(path, { // /upvoteIcon/{{this._id}}
             method: 'PATCH',
             headers: {
-            'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(data), // parse ID
         })
         if (res.status === 200) {
             location.reload(); // Refresh page
         }
-        }
-        catch(error){
+    } catch (error) { 
         console.error('Error:', error);
-        };
-    });
-});
+    };
+})});
+
+let commentUpvoteBtn = document.querySelectorAll('.commentUpvote');
+let commentDownvoteBtn = document.querySelectorAll('.commentDownvote');
+
+commentUpvoteBtn.forEach(button => {button.addEventListener("click", async event => {
+    console.log("I am here");
+    const clickedButtonId = event.target.id; // upvoteIcon/{{this._id}}
+    const data = {
+        _id: clickedButtonId.substring(17) // {{this._id}}
+    }
+
+    const path = "/" + clickedButtonId; // construct path
+
+    console.log(path);
+    try{
+        const res = await fetch(path, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // parse ID
+        })
+        if (res.status === 200) {
+        location.reload(); // Refresh page
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    };
+})});
+
+commentDownvoteBtn.forEach(button => {button.addEventListener("click", async event => {
+    console.log("I am here");
+    const clickedButtonId = event.target.id; // upvoteIcon/{{this._id}}
+    const data = {
+    _id: clickedButtonId.substring(18) // {{this._id}}
+    }
+
+    const path = "/" + clickedButtonId; // construct path
+
+    try{
+        const res = await fetch(path, { // /upvoteIcon/{{this._id}}
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data), // parse ID
+        })
+        if (res.status === 200) {
+            location.reload(); // Refresh page
+        }
+    } catch (error) {
+    console.error('Error:', error);
+    };
+})});
